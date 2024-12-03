b14e08f07435d352599d67f56139f732f4dad40c
alexo-senior <112290994+alexo-senior@users.noreply.github.com>
2024-12-02 22:06:00 -0500

    añadida la variable related_name para facilitar consultas

--- practica_modelos/company/models.py
+++ practica_modelos/company/models.py
@@ -1,5 +1,20 @@
 from django.db import models
+#los modelos deben tener las relaciones debidas entre ellos
+#para eso es bueno hacer el diagrama de flujo antes
+#al ir haciendo el codigo va mostrando por medio de errores
+#en la creacion de las varibles y relaciones cual debe ser primero
+""" Obtener todos los trabajos con un salario específico
+salary.jobs.all()
 
+    Obtener todas las ubicaciones de un país
+    country.locations.all()
+
+    Obtener todos los lugares en una ubicación
+    location.places.all()
+
+    Obtener todos los empleados en un lugar
+    place.employees.all()
+"""
 
 class Salary(models.Model):
     amount = models.IntegerField(null=False, blank=False)
@@ -7,15 +22,20 @@ class Salary(models.Model):
     extra_dic = models.BooleanField(default=False)
     
     def __str__(self):
-        self.amount
+        return str(self.amount)
 
 
 class Job(models.Model):
+    """ Ahora puedes hacer esto de forma más intuitiva
+    job = Job.objects.get(id=1)
+    empleados = job.employees.all()  # Más legible y semántico
+"""
     title = models.CharField(max_length=30, blank=False, null=False)
     description = models.TextField(null=True)
     #el trabajo tiene una relacion con el salario
-    salarys = models.ForeignKey(Salary, on_delete=models.CASCADE)
-        
+    salarys = models.ForeignKey(Salary, on_delete=models.CASCADE,related_name='jobs')
+    
+    
         
     def __str__(self):
         self.title
@@ -46,7 +66,7 @@ class Place(models.Model):
     address = models.CharField(max_length=50,null=False, blank=False)
     zip_code = models.CharField(max_length=5, null=False, blank=False)
     #el place va atener vinculacion con la location
-    locations = models.ForeignKey(Location, on_delete=models.CASCADE)
+    locations = models.ForeignKey(Location, on_delete=models.CASCADE, related_name='places')
     
         
     def __str__(self):
@@ -64,11 +84,12 @@ class Employee(models.Model):
     email = models.EmailField(max_length=50, blank=False, null=False)
     address = models.CharField(max_length=50, blank=False, null=False)
     #establece la clave foraneas en empleado es con job, el trabajo de esta persona
-    job = models.ForeignKey(Job, on_delete=models.CASCADE)
+    job = models.ForeignKey(Job, on_delete=models.CASCADE, related_name='employees')
     #la relacion con el lugar de trabajo
     places = models.ForeignKey(Place, on_delete=models.CASCADE)
     
     
     def __str__(self):
-        self.name    
+        return f"{self.name} {self.last_name}"   
+    
     
